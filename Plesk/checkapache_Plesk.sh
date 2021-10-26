a50e55fc8710929ef24ba96e50178fb41a7c7d16
w4zu <kevin@w4zu.ovh>
2021-10-26 15:30:51 +0200

    change apache check plesk

--- Plesk/checkapache_Plesk.sh
+++ Plesk/checkapache_Plesk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Check Apache via status page
-curl http://localhost:7079/server-status | grep prefork
+#curl http://localhost:7079/server-status | grep prefork
+curl http://localhost:7080/server-status |grep "Server MPM"
 if [ $? -ne 0 ]
 then
     service apache2 restart
